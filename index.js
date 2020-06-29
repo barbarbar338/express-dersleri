c541df29703d04dabded6163bc72fee9e52792a8
Barış Demirci <demirci.baris38@gmail.com>
2020-06-29 23:25:12 +0300

    [1-3] Express Dersleri - Middleware https://www.youtube.com/ProjectHammer

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use("/middleware", (req, res, next) => {
+    console.log(req.url);
+    next();
+});
+
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.send("Merhaba Dünya!");
+});
+
+app.get("/profil/:name", (req, res) => {
+    let name = req.params.name;
+    res.send(`${name} kullanıcısı profili: `);
+});
+
+app.get("/profil", (req, res) => {
+    let name = req.query.name;
+    if (!name) return res.send("bir kullanıcı adı belirtin.");
+    res.send(`${name} kullanıcısı profili: `);
+});
+
+app.listen(3000, () => {
+    console.log("Server port 3000'de hazır!")
+});
