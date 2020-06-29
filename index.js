e814c2095d3f201965975845147ffd4c03e165f9
Barış Demirci <demirci.baris38@gmail.com>
2020-06-29 23:24:25 +0300

    [1-2] Express Dersleri - Parametreler https://www.youtube.com/ProjectHammer

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.get("/", (req, res) => {
     res.send("Merhaba Dünya!");
 });
 
+app.get("/profil/:name", (req, res) => {
+    let name = req.params.name;
+    res.send(`${name} kullanıcısı profili: `);
+});
+
+app.get("/profil", (req, res) => {
+    let name = req.query.name;
+    if (!name) return res.send("bir kullanıcı adı belirtin.");
+    res.send(`${name} kullanıcısı profili: `);
+});
+
 app.listen(3000, () => {
     console.log("Server port 3000'de hazır!")
 });
