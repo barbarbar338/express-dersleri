77bb5fb23c450d2906691d5b7ce38a46aa0825fb
Barış Demirci <demirci.baris38@gmail.com>
2020-06-29 23:31:27 +0300

    [2-2] Express Dersleri - POST İşlemi 1 (TODO App) https://www.youtube.com/ProjectHammer

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
+const handlebarshelpers = require("handlebars-helpers");
+const app = express();
+
+// Görüntü motoru ayarları
+app.engine("handlebars", handlebars({
+    defaultLayout: "main",
+    layoutsDir: `${__dirname}/views/layouts/`,
+    helpers: handlebarshelpers()
+}));
+
+app.set("views", `${__dirname}/views/`);
+
+app.set("view engine", "handlebars");
+
+// body parser
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Static dosyalar
+app.use(express.static(__dirname + "/public"));
+
+// routerler
+app.use("/todo", require("./routers/todo"));
+
+// uygulamayı çalıştırma
+app.listen(3000, () => {
+    console.log("Uygulama 3000 portunda hazır!");
+});
